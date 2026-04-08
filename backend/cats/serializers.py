90123667ad495b1080e8f7a8fb30c591e2d39f4b
YosiaSol <leeevs@gmail.com>
2026-04-08 16:31:23 +0300

    Fix port for image, serializers.py

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -34,10 +34,16 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
         return super().to_internal_value(data)
+
+    def to_representation(self, value):
+        if not value:
+            return None
+        try:
+            return value.url
+        except ValueError:
+            return None
 
 
 class CatSerializer(serializers.ModelSerializer):
